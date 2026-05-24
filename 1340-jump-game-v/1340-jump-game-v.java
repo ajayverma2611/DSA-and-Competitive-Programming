7528df9a0033018cca1411b7d8db28b9d9cea0fd
ajayverma2611 <120312799+ajayverma2611@users.noreply.github.com>
2026-05-24 16:55:15 +0530

    Time: 11 ms (50.29%), Space: 46.4 MB (41.71%) - LeetHub

--- 1340-jump-game-v/1340-jump-game-v.java
+++ 1340-jump-game-v/1340-jump-game-v.java
@@ -2,55 +2,34 @@ class Solution {
 
     int[] dp;
 
-    public boolean check(int i, int j, int[] arr) {
-
-        if (arr[i] <= arr[j]) {
-            return false;
-        }
-
-        if (i < j) {
-
-            for (int k = i + 1; k < j; k++) {
-                if (arr[k] >= arr[i]) {
-                    return false;
-                }
-            }
-
-        } else {
-
-            for (int k = i - 1; k > j; k--) {
-                if (arr[k] >= arr[i]) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
-
     public int solve(int i, int[] arr, int n, int d) {
 
-        // memoization
         if (dp[i] != -1) {
             return dp[i];
         }
 
         int ans = 1;
 
-        // left
-        for (int j = i - 1; j >= i - d && j >= 0; j--) {
+        // move left
+        for (int j = i - 1; j >= 0 && j >= i - d; j--) {
 
-            if (check(i, j, arr)) {
-                ans = Math.max(ans, 1 + solve(j, arr, n, d));
+            // blocker found
+            if (arr[j] >= arr[i]) {
+                break;
             }
+
+            ans = Math.max(ans, 1 + solve(j, arr, n, d));
         }
 
-        // right
-        for (int j = i + 1; j <= i + d && j < n; j++) {
+        // move right
+        for (int j = i + 1; j < n && j <= i + d; j++) {
 
-            if (check(i, j, arr)) {
-                ans = Math.max(ans, 1 + solve(j, arr, n, d));
+            // blocker found
+            if (arr[j] >= arr[i]) {
+                break;
             }
+
+            ans = Math.max(ans, 1 + solve(j, arr, n, d));
         }
 
         return dp[i] = ans;
